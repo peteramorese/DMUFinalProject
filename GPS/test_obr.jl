2b5a9a170883979d0fe8e3411ac29f49a9b8c9fd
Peter Amorese <peter.amorese@gmail.com>
2022-04-19 15:06:34 -0600

    Added missing modules

--- GPS/test_obr.jl
+++ GPS/test_obr.jl
@@ -1,12 +1,14 @@
+using LightGraphs
+using StaticArrays
 include("./GridWorldGraph.jl")
 
 # Create Deterministic Grid World
-dgw = DeterministicGridWorld(grid_size_x=3, grid_size_y=3)
+dgw = GridWorldGraph.DeterministicGridWorld(grid_size_x=3, grid_size_y=3)
 println(vertices(dgw.G))
 
 # Test update edge weight using state and action
 println(dgw.W)
-didwork = update_edge_weight!(dgw, SVector{2,Int}(1,2), "up", 1.2)
+didwork = GridWorldGraph.update_edge_weight!(dgw, SVector{2,Int}(1,2), "up", 1.2)
 println(dgw.W)
 
 
