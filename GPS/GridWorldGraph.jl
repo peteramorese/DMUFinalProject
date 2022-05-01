079a37a1939a93a8480938a47d06d3f7513c4575
Joe Miceli <joe.miceli8@gmail.com>
2022-05-01 15:30:34 -0600

    Significant clean up, moved visualization to its own file, addressed comments and removed unecessary items, added randomness to MDP transition function

--- GPS/GridWorldGraph.jl
+++ GPS/GridWorldGraph.jl
@@ -1,50 +1,126 @@
+#=========================================================
+# ASEN5519 Decision Making Under Uncertainty
+# Spring 2022
+# Final Project
+#  
+# Peter Amorese
+# Kyle Li 
+# Joe Miceli
+#   
+# =========================================================#
+# GridWorldGraph
+#   <file description>
+# =========================================================#
 
 module GridWorldGraph
 
-export DeterministicGridWorld
+    using LabelledGraphs
+    using LightGraphs
+    using StaticArrays
 
-using LabelledGraphs
-using LightGraphs
-using StaticArrays
+    export DeterministicGridWorld
 
-# TODO: make this objet a field of the GlobalGPSCarWorld struct
-# TODO: remove the actions field from here
-mutable struct DeterministicGridWorld
-    G::LabelledGraph # Graph
-    rG::LabelledGraph # Reverse Graph
-    W::Dict{String, Float64}
-    updated::Dict{String, Bool}
-    #rW::Dict{String, Float64}
-    grid_size_x::Int
-    grid_size_y::Int
-    actions::Vector{Symbol}
-end
-
-function state_lbls_to_edge_lbl(src::String, dst::String)
-    return string(string(src), "_", string(dst))
-end
-
-function labeled_edge_to_str(edge::LabelledEdge)
-    return string(string(edge.src), "_", string(edge.dst))
-end
-
-function create_grid!(graph::LabelledGraph, r_graph::LabelledGraph, grid_size_x::Int64, grid_size_y::Int64)
-    function safe_add_vertex!(graph, v)
-        if !has_vertex(graph, v)
-            add_vertex!(graph, v)
-        end
+    # TODO: remove the actions field from here
+    mutable struct DeterministicGridWorld
+        G::LabelledGraph # Graph
+        rG::LabelledGraph # Reverse Graph
+        W::Dict{String, Float64}
+        updated::Dict{String, Bool}
+        #rW::Dict{String, Float64}
+        grid_size_x::Int
+        grid_size_y::Int
+        actions::Vector{Symbol}
     end
-    edge_weights = Dict{String, Float64}()
-    for i=1:grid_size_x
-        for ii=1:grid_size_y
-            src_state = string(string(i),",", string(ii))
-            safe_add_vertex!(graph, src_state)
-            safe_add_vertex!(r_graph, src_state)
-            for dir=1:4
-                stay_put = false
-                if dir == 1 # left
-                    if i != 1
-                        dst_state = string(string(i-1),",", string(ii))
+
+    function state_lbls_to_edge_lbl(src::String, dst::String)
+        return string(string(src), "_", string(dst))
+    end
+
+    function labeled_edge_to_str(edge::LabelledEdge)
+        return string(string(edge.src), "_", string(edge.dst))
+    end
+
+    function create_grid!(graph::LabelledGraph, r_graph::LabelledGraph, grid_size_x::Int64, grid_size_y::Int64)
+        function safe_add_vertex!(graph, v)
+            if !has_vertex(graph, v)
+                add_vertex!(graph, v)
+            end
+        end
+        edge_weights = Dict{String, Float64}()
+        for i=1:grid_size_x
+            for ii=1:grid_size_y
+                src_state = string(string(i),",", string(ii))
+                safe_add_vertex!(graph, src_state)
+                safe_add_vertex!(r_graph, src_state)
+                for dir=1:4
+                    stay_put = false
+                    if dir == 1 # left
+                        if i != 1
+                            dst_state = string(string(i-1),",", string(ii))
+                            safe_add_vertex!(graph, dst_state)
+                            safe_add_vertex!(r_graph, dst_state)
+                            
+                            # Weight the edge
+                            edge_label = state_lbls_to_edge_lbl(src_state, dst_state)
+                            edge_weights[edge_label] = 1.0 # Init value
+
+                            # Connect the graph
+                            add_edge!(graph, src_state, dst_state)
+                            add_edge!(r_graph, dst_state, src_state)
+                        else
+                            stay_put = true
+                        end
+                    elseif dir == 2 # right
+                        if i != grid_size_x
+                            dst_state = string(string(i+1),",", string(ii))
+                            safe_add_vertex!(graph, dst_state)
+                            safe_add_vertex!(r_graph, dst_state)
+                            
+                            # Weight the edge
+                            edge_label = state_lbls_to_edge_lbl(src_state, dst_state)
+                            edge_weights[edge_label] = 1.0 # Init value
+
+                            # Connect the graph
+                            add_edge!(graph, src_state, dst_state)
+                            add_edge!(r_graph, dst_state, src_state)
+                        else
+                            stay_put = true
+                        end
+                    elseif dir == 3 # down
+                        if ii != 1
+                            dst_state = string(string(i),",", string(ii-1))
+                            safe_add_vertex!(graph, dst_state)
+                            safe_add_vertex!(r_graph, dst_state)
+                            
+                            # Weight the edge
+                            edge_label = state_lbls_to_edge_lbl(src_state, dst_state)
+                            edge_weights[edge_label] = 1.0 # Init value
+
+                            # Connect the graph
+                            add_edge!(graph, src_state, dst_state)
+                            add_edge!(r_graph, dst_state, src_state)
+                        else
+                            stay_put = true
+                        end
+                    elseif dir == 4 # up
+                        if ii != grid_size_y
+                            dst_state = string(string(i),",", string(ii+1))
+                            safe_add_vertex!(graph, dst_state)
+                            safe_add_vertex!(r_graph, dst_state)
+                            
+                            # Weight the edge
+                            edge_label = state_lbls_to_edge_lbl(src_state, dst_state)
+                            edge_weights[edge_label] = 1.0 # Init value
+
+                            # Connect the graph
+                            add_edge!(graph, src_state, dst_state)
+                            add_edge!(r_graph, dst_state, src_state)
+                        else
+                            stay_put = true
+                        end
+                    end
+                    if stay_put
+                        dst_state = src_state
                         safe_add_vertex!(graph, dst_state)
                         safe_add_vertex!(r_graph, dst_state)
                         
@@ -55,168 +131,104 @@ function create_grid!(graph::LabelledGraph, r_graph::LabelledGraph, grid_size_x:
                         # Connect the graph
                         add_edge!(graph, src_state, dst_state)
                         add_edge!(r_graph, dst_state, src_state)
-                    else
-                        stay_put = true
                     end
-                elseif dir == 2 # right
-                    if i != grid_size_x
-                        dst_state = string(string(i+1),",", string(ii))
-                        safe_add_vertex!(graph, dst_state)
-                        safe_add_vertex!(r_graph, dst_state)
-                        
-                        # Weight the edge
-                        edge_label = state_lbls_to_edge_lbl(src_state, dst_state)
-                        edge_weights[edge_label] = 1.0 # Init value
-
-                        # Connect the graph
-                        add_edge!(graph, src_state, dst_state)
-                        add_edge!(r_graph, dst_state, src_state)
-                    else
-                        stay_put = true
-                    end
-                elseif dir == 3 # down
-                    if ii != 1
-                        dst_state = string(string(i),",", string(ii-1))
-                        safe_add_vertex!(graph, dst_state)
-                        safe_add_vertex!(r_graph, dst_state)
-                        
-                        # Weight the edge
-                        edge_label = state_lbls_to_edge_lbl(src_state, dst_state)
-                        edge_weights[edge_label] = 1.0 # Init value
-
-                        # Connect the graph
-                        add_edge!(graph, src_state, dst_state)
-                        add_edge!(r_graph, dst_state, src_state)
-                    else
-                        stay_put = true
-                    end
-                elseif dir == 4 # up
-                    if ii != grid_size_y
-                        dst_state = string(string(i),",", string(ii+1))
-                        safe_add_vertex!(graph, dst_state)
-                        safe_add_vertex!(r_graph, dst_state)
-                        
-                        # Weight the edge
-                        edge_label = state_lbls_to_edge_lbl(src_state, dst_state)
-                        edge_weights[edge_label] = 1.0 # Init value
-
-                        # Connect the graph
-                        add_edge!(graph, src_state, dst_state)
-                        add_edge!(r_graph, dst_state, src_state)
-                    else
-                        stay_put = true
-                    end
-                end
-                if stay_put
-                    dst_state = src_state
-                    safe_add_vertex!(graph, dst_state)
-                    safe_add_vertex!(r_graph, dst_state)
-                    
-                    # Weight the edge
-                    edge_label = state_lbls_to_edge_lbl(src_state, dst_state)
-                    edge_weights[edge_label] = 1.0 # Init value
-
-                    # Connect the graph
-                    add_edge!(graph, src_state, dst_state)
-                    add_edge!(r_graph, dst_state, src_state)
                 end
             end
         end
+        return edge_weights
     end
-    return edge_weights
-end
 
-# CTOR
-function DeterministicGridWorld(;grid_size_x=10,grid_size_y=10)
-    println("Creating graph...")
-    g_ = path_digraph(1)
-    r_g_ = path_digraph(1)
-    G = LabelledGraph([""], g_)
-    r_G = LabelledGraph([""], r_g_)
-    W = create_grid!(G, r_G, grid_size_x, grid_size_y)
-    updated = Dict{String, Bool}()
-    for e in keys(W)
-        updated[e] = false 
-    end
-    actions = [:left, :right, :up, :down]   # TODO: make this an argument of the constructor
-    DeterministicGridWorld(G, r_G, W, updated, grid_size_x, grid_size_y, actions)
-end
-
-function state_to_str(state::SVector{2,Int})
-    return string(string(state[1]),",",string(state[2]))
-end
-
-function state_lbls_to_edge_lbl(src_state::SVector{2,Int}, dst_state::SVector{2,Int})
-    return string(state_to_str(src_state), "_", state_to_str(dst_state))
-end
-
-function str_to_state(state_str::String)
-    sub_str = split(state_str, ",")
-    state = SVector{2, Int}(parse(Int64, sub_str[1]), parse(Int64, sub_str[2]))
-    return state
-end
-
-function update_edge_weight!(dgw::DeterministicGridWorld, edge::String, weight::Float64)
-    if haskey(dgw.W, edge)  
-        if !dgw.updated[edge]
-            dgw.W[edge] = weight
-            dgw.updated[edge] = true
+    # CTOR
+    function DeterministicGridWorld(;grid_size_x=10,grid_size_y=10)
+        println("Creating graph...")
+        g_ = path_digraph(1)
+        r_g_ = path_digraph(1)
+        G = LabelledGraph([""], g_)
+        r_G = LabelledGraph([""], r_g_)
+        W = create_grid!(G, r_G, grid_size_x, grid_size_y)
+        updated = Dict{String, Bool}()
+        for e in keys(W)
+            updated[e] = false 
         end
-        return true
-    else
-        return false
+        actions = [:left, :right, :up, :down]   # TODO: make this an argument of the constructor
+        DeterministicGridWorld(G, r_G, W, updated, grid_size_x, grid_size_y, actions)
     end
-end
 
-function update_edge_weight!(dgw::DeterministicGridWorld, state::SVector{2,Int}, action::Symbol, weight::Float64)
-    # Check if action is allowed:
-    if action == :left
-        if state[1] == 1
-            dst_state = state
+    function state_to_str(state::SVector{2,Int})
+        return string(string(state[1]),",",string(state[2]))
+    end
+
+    function state_lbls_to_edge_lbl(src_state::SVector{2,Int}, dst_state::SVector{2,Int})
+        return string(state_to_str(src_state), "_", state_to_str(dst_state))
+    end
+
+    function str_to_state(state_str::String)
+        sub_str = split(state_str, ",")
+        state = SVector{2, Int}(parse(Int64, sub_str[1]), parse(Int64, sub_str[2]))
+        return state
+    end
+
+    function update_edge_weight!(dgw::DeterministicGridWorld, edge::String, weight::Float64)
+        if haskey(dgw.W, edge)  
+            if !dgw.updated[edge]
+                dgw.W[edge] = weight
+                dgw.updated[edge] = true
+            end
+            return true
         else
-            dst_state = SVector{2,Int}(state[1] - 1, state[2])
-        end
-    elseif action == :right
-        if state[1] == dgw.grid_size_x
-            dst_state = state
-        else
-            dst_state = SVector{2,Int}(state[1] + 1, state[2])
-        end
-    elseif action == :down
-        if state[2] == 1
-            dst_state = state
-        else
-            dst_state = SVector{2,Int}(state[1], state[2] - 1)
-        end
-    elseif action == :up
-        if state[2] == dgw.grid_size_y
-            dst_state = state
-        else
-            dst_state = SVector{2,Int}(state[1], state[2] + 1)
-        end
-    else
-        return false
-    end
-    edge = state_lbls_to_edge_lbl(state_to_str(state), state_to_str(dst_state))
-    if haskey(dgw.W, edge) 
-        if !dgw.updated[edge]
-            dgw.W[edge] = weight
-            dgw.updated[edge] = true
-        end
-        return true
-    else
-        return false
-    end
-end
-
-function update_state_weight!(dgw::DeterministicGridWorld, state::SVector{2,Int}, weight::Float64) 
-    for a in dgw.actions
-        if !update_edge_weight!(dgw, state, a, weight)
-            println("Failed to update edge. State: ", state, " action ", a)
             return false
         end
     end
-    return true
-end
 
-end
+    function update_edge_weight!(dgw::DeterministicGridWorld, state::SVector{2,Int}, action::Symbol, weight::Float64)
+        # Check if action is allowed:
+        if action == :left
+            if state[1] == 1
+                dst_state = state
+            else
+                dst_state = SVector{2,Int}(state[1] - 1, state[2])
+            end
+        elseif action == :right
+            if state[1] == dgw.grid_size_x
+                dst_state = state
+            else
+                dst_state = SVector{2,Int}(state[1] + 1, state[2])
+            end
+        elseif action == :down
+            if state[2] == 1
+                dst_state = state
+            else
+                dst_state = SVector{2,Int}(state[1], state[2] - 1)
+            end
+        elseif action == :up
+            if state[2] == dgw.grid_size_y
+                dst_state = state
+            else
+                dst_state = SVector{2,Int}(state[1], state[2] + 1)
+            end
+        else
+            return false
+        end
+        edge = state_lbls_to_edge_lbl(state_to_str(state), state_to_str(dst_state))
+        if haskey(dgw.W, edge) 
+            if !dgw.updated[edge]
+                dgw.W[edge] = weight
+                dgw.updated[edge] = true
+            end
+            return true
+        else
+            return false
+        end
+    end
+
+    function update_state_weight!(dgw::DeterministicGridWorld, state::SVector{2,Int}, weight::Float64) 
+        for a in dgw.actions
+            if !update_edge_weight!(dgw, state, a, weight)
+                println("Failed to update edge. State: ", state, " action ", a)
+                return false
+            end
+        end
+        return true
+    end
+
+end #= GridWorldGraph =#
--- GPSCarSimulation.jl
+++ GPSCarSimulation.jl
@@ -6,99 +6,168 @@
 # Peter Amorese
 # Kyle Li 
 # Joe Miceli
-=========================================================#
+#   
+# =========================================================#
+# GPSCarSimulation
+#   This file contains the main simulation loop for the 
+#   GPS car equiped with a local LIDAR sensor. A global 
+#   planner determines the path to goal without knowledge
+#   of bad roads, a local MDP is created each step that 
+#   combines knoweldge of bad roads with the global plan
+#   to determine which action to take. The global plan is 
+#   then updated using another local MDP that only accounts
+#   for bad roads (and ignores the global plan). 
+# =========================================================#
 
+# Set up
+cd(dirname(@__FILE__()))    # Ensures outputs will appear in the same folder as code
+CURRENT_DIR = pwd();        # Current directory
 
-# include("./GPSCarFinalProject.jl")
+# Custom modules
+include("./GPSCarFinalProject.jl")
+include("GPS/OptimalBackwardsReachability.jl")
+include("./GPSCarVisualization.jl")
 
 using .GPSCarFinalProject
+using .OBReachability
 
+# Julia modules
+using Revise
 using POMDPs
 using POMDPModelTools
 using POMDPPolicies
 using POMDPSimulators
 using StaticArrays
 using DiscreteValueIteration
+using Plots
 
-# gridWorld = GlobalGPSCarWorld()
-
-# m = LocalGPSCarMDP(gridWorld)
-# p = RandomPolicy(m)
-
-# @show reward = simulate(RolloutSimulator(max_steps=10),m,p)
-
-#=
-TODO: Main routine will need to be something like
-    1) Initalize global grid world
-    2) Compute naive path to goal
-    3) Initalize and solve local MDP
-    4) Take a step using the action calculated by solving MDP
-    5) Update global grid world
-    6) repeat steps 2-5 until terminated
-=#
-
-# 1) Initialize grid world
-@show gridWorld = GlobalGPSCarWorld(numObstacles=10, numBadRoads=10)
-
-# gridWorld = GlobalGPSCarWorld()
-println("INIT POSTION: ", gridWorld.carPosition)
-
-# Create solver to be used on local MDP
-localSolver = ValueIterationSolver(max_iterations=100, belres=1e-6)
-
-# Initialize accumulated reward
-global totalReward = 0.0
-
-# 6) repeat steps 2-5 until terminated
-while gridWorld.carPosition != gridWorld.goalPosition
+import GridWorlds as GW
 
 
-    # 2) Compute naive path to goal
-    #   pathToGoal is a field of GlobalGPSCarWorld object of type SVector{2, Int}[] (an array of SVectors)
-    #   it should contain an ordered set of states that take the car from it's current state to the goal
+# Main simulation routine
+function GPSCarSimulation(gridWorld::GlobalGPSCarWorld; maxSteps = 10000)
+    println("BEGINNING GPS CAR SIMULATION...")
+    println("INIT POSTION: ", gridWorld.carPosition)
 
-    # THIS IS A TEST PATH
-    local testPathToGoal = [
-    gridWorld.carPosition,  # 2,2
-    SVector(2,3),
-    SVector(2,4),
-    SVector(2,5),
-    SVector(2,6),
-    SVector(2,7),
-    SVector(2,8),
-    SVector(2,9),
-    SVector(2,10),
-    SVector(3,10),
-    SVector(4,10),
-    SVector(5,10),
-    SVector(6,10),
-    SVector(7,10),
-    SVector(8,10),
-    SVector(9,10),
-    SVector(10,10)]
-
-    gridWorld.pathToGoal = testPathToGoal
+    # Create a goal position vector for backward reachability algorithm
+    goalPositionVec = Vector{SVector{2, Int}}()
+    push!(goalPositionVec, gridWorld.goalPosition)
     
-    # 3) Initialize and solve the local MDP
-    local sensorRadius = 1  # the distance in grid world the car can sense around itself
-    local localMDP = LocalGPSCarMDP(gridWorld, gridRadius = sensorRadius)
-    println("LOCAL STATES: ", states(localMDP))
-    local localPolicy = solve(localSolver, localMDP)
+    # Create solver to be used on local MDP
+    localSolver = ValueIterationSolver(max_iterations=100, belres=1e-6)
 
-    # 4) Take a step using the action calculated by solving MDP
-    local carAction = action(localPolicy, GPSCarState(gridWorld.carPosition))
+    # Initialize accumulated reward and trajectory
+    totalReward = 0.0
+    trajectory = Vector{SVector{2, Int}}()
+    push!(trajectory, gridWorld.carPosition)
+
+    # While not at the goal or current step less than max number of steps
+    while (gridWorld.carPosition != gridWorld.goalPosition) && (length(trajectory)<maxSteps)
+
+        # Set weights using backward reachability 
+        weights = OBReachability.obr(gridWorld.graph, goalPositionVec)
+
+        # Create the local MDP based on the states visible to the car and the weights from the global planner
+        local sensorRadius = 1  # the distance in grid world the car can sense around itself    # TODO: make this an argument of GlobalGPSCarWorld
+        local localMDP = LocalGPSCarMDP(gridWorld, weights, gridRadius = sensorRadius)
+        
+        # Solve the local MDP
+        local localPolicy = solve(localSolver, localMDP)
+
+        # Solve the local MDP without knowledge of the global planner
+        # The point of this is to remove circular dependency (global depends on local which depends on global...)
+        localMDP_zeroStateWeights =  LocalGPSCarMDP(gridWorld, weights, gridRadius = sensorRadius)
+        for s in states(localMDP_zeroStateWeights)
+            localMDP_zeroStateWeights.stateWeights[s.car] = 0.0
+        end
+        local localPolicy_zeroStateWeights = solve(localSolver, localMDP_zeroStateWeights)
 
 
-    local carActionDirection = actiondir[carAction]
+        # Take a step using the action calculated by solving the globally-influenced local MDP
+        # TODO: turn this into an "act!" function
+        println("\nPRINTING RELEVANT WEIGHTS: ")
+        for s in states(localMDP)
+            println("  state: ", s.car, " weight: ", weights[s.car])
+        end
+        local carAction = action(localPolicy, GPSCarState(gridWorld.carPosition))
 
-    # 5) Update global grid world
-    gridWorld.carPosition = gridWorld.carPosition + carActionDirection
-    println("NEW POSTION: ", gridWorld.carPosition)
+        println("CURR POSTION: ", gridWorld.carPosition, " CAR ACTION: ", carAction)
+        sp = rand(transition(localMDP, GPSCarState(gridWorld.carPosition), carAction))
+        gridWorld.carPosition = sp.car
+        println("NEW POSTION: ", gridWorld.carPosition)
 
-    local rewardForAction = reward(localMDP, GPSCarState(gridWorld.carPosition), carAction, GPSCarState(gridWorld.carPosition))
-    global totalReward += rewardForAction
-    println("Accumulated Reward: ", totalReward)
+        # Collect the reward from this transition
+        totalReward += reward(localMDP_zeroStateWeights, gridWorld.carPosition, carAction, sp)
 
-end
+        # Update state weights in the global planner using Q(s,a)
+        # Note that the weights going into and coming out of obr must be positive 
+        for s in states(localMDP)
+    
+            # Using the solution without obr bias
+            Qsa = value(localPolicy_zeroStateWeights, s, carAction)
 
+            println("Q val s: ", s, " value: ", Qsa) 
+            success = GPSCarFinalProject.GridWorldGraph.update_state_weight!(gridWorld.graph, s.car, -Qsa)
+
+            if !success
+                println("Failed to update state weights")
+                return 0
+            end
+        end
+        
+        push!(trajectory, gridWorld.carPosition)
+    end
+
+
+    return totalReward, trajectory
+end #= GPSCarSimulation =#
+
+
+
+
+function main()
+    # 1st gridworld definition
+    gridWorldsize = SVector(10,10)
+    initPosition = SVector(3,6)
+    goalPosition = SVector(9,2)
+
+    obstacles = [RectangleObstacle(SVector(4,5), SVector(6,9)), RectangleObstacle(SVector(8,1), SVector(8,5)), RectangleObstacle(SVector(1,9), SVector(2,10)),
+                RectangleObstacle(SVector(9,9), SVector(10,10)), RectangleObstacle(SVector(1,1), SVector(2,2))]
+    badRoads = [RectangleObstacle(SVector(5,2), SVector(5,4)), RectangleObstacle(SVector(6,2), SVector(7,2))]
+
+    function mapDown(mdp_reward, obr_cost)
+        return mdp_reward - obr_cost^2
+    end
+
+    gridWorld = GlobalGPSCarWorld(obstacles, badRoads, mapDown, size = gridWorldsize, initPosition = initPosition, goalPosition = goalPosition)
+
+    # Simulate 1st grid world
+    totalReward, trajectory = GPSCarSimulation(gridWorld)
+    
+    # Debugging: print trajectory information
+    if length(trajectory) < 50
+        println("Trajectory: ", trajectory)
+    else
+        println("Trajectory too long to print.")
+    end
+    for pos in trajectory
+        if gridWorld.badRoads[pos]
+            println("Trajectory enters bad road: ", pos)
+        elseif gridWorld.obstacles[pos]
+            println("Trajectory enters obstacle: ", pos)
+        end
+    end
+
+    # Generate visualization from simulation of 1st grid world
+    GPSCarVisualization(gridWorld, trajectory, pngFileName = "GridWorld_1_Plot.png", gifFileName = "GridWorld_1_Trajectory.gif")
+
+
+end #= main =#
+
+# ====================================================================================================
+main()
+
+
+
+   
 
